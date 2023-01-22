56c1989590fcdac15a6776db91597639bc4628ec
giri-vardhan <122589788+giri-vardhan@users.noreply.github.com>
2023-01-22 12:34:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -197,13 +197,7 @@ func main() {
 }
 
 // curl localhost:8080/posts -i -H "Content-Type: application/json" -d @body.json --request "POST"
-/*
-curl http://localhost:8080/albums \
---include \
---header "Content-Type: application/json" \
---request "POST" \
---data '{"id": "4","title": "The Modern Sound of Betty Carter","artist": "Betty Carter","price": 49.99}'
-*/
+
 
 /*
 curl http://localhost:8080/comment \
